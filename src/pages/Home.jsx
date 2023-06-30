7c9af8f9335c69b9720df125e80f08231e2b6b33
Janela Daculong <castulojanela@gmail.com>
2023-07-01 00:32:23 +0800

    Change font color to hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
                             <h1 className="hero-header2 display-5">One pixel at a time.</h1>
                             <h1 className="hero-header3 display-5">Hi there, I'm Janela!</h1>
                         </Container>
-                        <p className='hero-text text-white fs-5'>Hello and welcome! My name is Janela Daculong,
+                        <p className='hero-text text-dark fs-5'>Hello and welcome! My name is Janela Daculong,
                             and I'm a self-directed Full-Stack Web Developer with a strong foundation in web development tools and frameworks.
                             With a passion for creating beautiful and functional websites, I've worked on several
                             projects to showcase my skills and abilities. Take a look at my portfolio to see some of my
